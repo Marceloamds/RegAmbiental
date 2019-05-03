0442c8633a396f558de5fb3bdea4042ee102dc00
Diego Segovia <garcia.segovia171@gmail.com>
2019-05-03 10:50:19 -0400

    Telas criadas

---
+++ app/src/main/java/com/engefour/regambiental/DetalhesActivity.kt
@@ -0,0 +1,12 @@
+package com.engefour.regambiental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+}
