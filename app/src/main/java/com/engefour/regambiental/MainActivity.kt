c255c77e1c13ec965821ebb451d18e99801a2d5d
Diego Segovia <garcia.segovia171@gmail.com>
2019-05-04 13:54:06 -0400

    Main e Cadastro feitas

--- app/src/main/java/com/engefour/regambiental/MainActivity.kt
+++ app/src/main/java/com/engefour/regambiental/MainActivity.kt
@@ -2,11 +2,20 @@ package com.engefour.regambiental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var botao = findViewById(R.id.botaoId)
+//        botao.setOn
+
+
+
+
+
     }
 }
